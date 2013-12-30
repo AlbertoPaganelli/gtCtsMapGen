9b04cfe006602794449ac4f57e0efd08a08697bd
AlbertoPaganelli <albi87@gmail.com>
2013-12-30 10:11:41 +0100

    First commit

---
+++ code/AstroI.h
@@ -0,0 +1,24 @@
+#ifndef __AstroI_h__
+#define __AstroI_h__
+
+#include <Astro.h>
+
+namespace Astro
+{
+
+class AgileCtsMapGenI : virtual public AgileCtsMapGen
+{
+public:
+
+    virtual ::Astro::Matrix calculateMapKey(const ::Astro::AGILECtsMapGenParams&,
+                                            const Ice::Current&);
+
+    virtual void shutdown(const Ice::Current&);
+
+//private:
+//    bool LogEvtString(AGILECtsMapGenParams params, std::vector<float>& ra, std::vector<float>& dec);
+};
+
+}
+
+#endif
---
+++ code/LogEvt.h
@@ -0,0 +1,13 @@
+/*
+ * LogEvt.h
+ *
+ *  Created on: Dec 30, 2013
+ *      Author: Alberto Paganelli
+ */
+
+#ifndef LOGEVT_H_
+#define LOGEVT_H_
+
+ bool LogEvtString(Astro::AGILECtsMapGenParams params, std::vector<float>& ra, std::vector<float>& dec);
+
+#endif /* LOGEVT_H_ */
